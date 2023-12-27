96580211823d9d6cfe464dc6b898e1be19c21af8
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2023-12-27 00:47:01 -0300

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PageContentComponent } from './Components/page-content/page-content.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path : "teste", component : PageContentComponent},
+    {path: "normal", component : FooterComponent},
+];
